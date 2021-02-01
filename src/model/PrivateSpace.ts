06ff3ef1f6e66827ec3c6198a89eccf794a2e0c2
Wighawag <wighawag@gmail.com>
2021-02-01 20:32:24 +0000

    tweaks

--- src/model/PrivateSpace.ts
+++ src/model/PrivateSpace.ts
@@ -23,6 +23,27 @@ export class PrivateSpace extends Space {
     return this.privateAccount.getFleets();
   }
 
+  timeLeftForFleet(
+    time: number,
+    fleet: OwnFleet
+  ): {timeLeft: number; timePassed: number; fullTime: number} {
+    const fromPlanet = this.ensurePlanetAt(fleet.from.x, fleet.from.y);
+    const toPlanet = this.ensurePlanetAt(fleet.to.x, fleet.to.y);
+    const gFromX = fromPlanet.location.globalX;
+    const gFromY = fromPlanet.location.globalY;
+    const gToX = toPlanet.location.globalX;
+    const gToY = toPlanet.location.globalY;
+    const speed = fromPlanet.stats.speed;
+    const fullDistance = Math.floor(
+      Math.sqrt(Math.pow(gToX - gFromX, 2) + Math.pow(gToY - gFromY, 2))
+    );
+    const fullTime =
+      fullDistance * ((this.spaceInfo.timePerDistance * 10000) / speed);
+    const timePassed = time - fleet.launchTime;
+    const timeLeft = fullTime - timePassed;
+    return {timeLeft, timePassed, fullTime};
+  }
+
   isTxPerformed(txHash?: string): boolean {
     return this.privateAccount.isTxPerformed(txHash);
   }
